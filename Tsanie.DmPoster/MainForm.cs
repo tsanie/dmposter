f39fd2f3e710124065276ece4de9938deaeace43
tsorgy@gmail.com <tsorgy@gmail.com@41d5888c-bf74-9242-a842-63dfd04b6711>
2011-08-08 01:22:15 +0000

    add: 添加各个组件项目

--- Tsanie.DmPoster/MainForm.cs
+++ Tsanie.DmPoster/MainForm.cs
@@ -9,17 +9,40 @@ using System.Windows.Forms;
 
 namespace Tsanie.DmPoster {
     public partial class MainForm : Form {
+
+        #region - 构造 -
+
         public MainForm() {
             InitializeComponent();
             this.Icon = Tsanie.DmPoster.Properties.Resources.AppIcon;
+            this.Text = Language.Untitled + " - " + Config.Title;
         }
+
+        #endregion
+
+        #region - 私有方法 -
+
+        private void ShowMessage(string message, string title, MessageBoxButtons buttons, MessageBoxIcon icon) {
+            MessageBox.Show(this, message, title, buttons, icon);
+        }
+
+        #endregion
 
         private void Command_OnAction(object sender, EventArgs e) {
             string command = (sender as ToolStripItem).Tag as string;
             switch (command) {
-                case "New":
+                case "Exit":
+                    this.Close();
+                    Application.Exit();
+                    break;
+                default:
+                    this.ShowMessage("未实现: " + command, "事件触发", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     break;
             }
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e) {
+
         }
     }
 }
---
+++ Tsanie.UI/ProgressTaskbar.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Tsanie.UI {
+    /// <summary>
+    /// ProgressTaskbar provides Windows 7 taskbar progress
+    /// </summary>
+    [ComImportAttribute()]
+    [GuidAttribute("ea1afb91-9e28-4b86-90e9-9e9f8a5eefaf")]
+    [InterfaceTypeAttribute(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface ITaskbarList3 {
+        [PreserveSig]
+        void HrInit();
+        [PreserveSig]
+        void AddTab(IntPtr hwnd);
+        [PreserveSig]
+        void DeleteTab(IntPtr hwnd);
+        [PreserveSig]
+        void ActivateTab(IntPtr hwnd);
+        [PreserveSig]
+        void SetActiveAlt(IntPtr hwnd);
+
+        [PreserveSig]
+        void MarkFullscreenWindow(IntPtr hwnd, [MarshalAs(UnmanagedType.Bool)] bool fFullscreen);
+
+        void SetProgressValue(IntPtr hwnd, UInt64 ullCompleted, UInt64 ullTotal);
+        void SetProgressState(IntPtr hwnd, TBPFLAG tbpFlags);
+        void RegisterTab(IntPtr hwndTab, IntPtr hwndMDI);
+        void UnregisterTab(IntPtr hwndTab);
+        void SetTabOrder(IntPtr hwndTab, IntPtr hwndInsertBefore);
+        void SetTabActive(IntPtr hwndTab, IntPtr hwndMDI, TBATFLAG tbatFlags);
+        void ThumbBarAddButtons(IntPtr hwnd, uint cButtons, [MarshalAs(UnmanagedType.LPArray)] THUMBBUTTON[] pButtons);
+        void ThumbBarUpdateButtons(IntPtr hwnd, uint cButtons, [MarshalAs(UnmanagedType.LPArray)] THUMBBUTTON[] pButtons);
+        void ThumbBarSetImageList(IntPtr hwnd, IntPtr himl);
+        void SetOverlayIcon(IntPtr hwnd, IntPtr hIcon, [MarshalAs(UnmanagedType.LPWStr)] string pszDescription);
+        void SetThumbnailTooltip(IntPtr hwnd, [MarshalAs(UnmanagedType.LPWStr)] string pszTip);
+        void SetThumbnailClip(IntPtr hwnd, ref RECT prcClip);
+    }
+
+    [GuidAttribute("56FDF344-FD6D-11d0-958A-006097C9A090")]
+    [ClassInterfaceAttribute(ClassInterfaceType.None)]
+    [ComImportAttribute()]
+    public class ProgressTaskbar { }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public struct THUMBBUTTON {
+        [MarshalAs(UnmanagedType.U4)]
+        public THBMASK dwMask;
+        public uint iId;
+        public uint iBitmap;
+        public IntPtr hIcon;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+        public string szTip;
+        [MarshalAs(UnmanagedType.U4)]
+        public THBFLAGS dwFlags;
+    }
+
+    public enum THBMASK {
+        THB_BITMAP = 0x1,
+        THB_ICON = 0x2,
+        THB_TOOLTIP = 0x4,
+        THB_FLAGS = 0x8
+    }
+
+    public enum THBFLAGS {
+        THBF_ENABLED = 0,
+        THBF_DISABLED = 0x1,
+        THBF_DISMISSONCLICK = 0x2,
+        THBF_NOBACKGROUND = 0x4,
+        THBF_HIDDEN = 0x8
+    }
+
+    public enum TBPFLAG {
+        TBPF_NOPROGRESS = 0,
+        TBPF_INDETERMINATE = 0x1,
+        TBPF_NORMAL = 0x2,
+        TBPF_ERROR = 0x4,
+        TBPF_PAUSED = 0x8
+    }
+
+    public enum TBATFLAG {
+        TBATF_USEMDITHUMBNAIL = 0x1,
+        TBATF_USEMDILIVEPREVIEW = 0x2
+    }
+
+    public struct RECT {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+
+        public RECT(int left, int top, int right, int bottom) {
+            this.left = left;
+            this.top = top;
+            this.right = right;
+            this.bottom = bottom;
+        }
+    }
+}
